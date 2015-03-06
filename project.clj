6abdfbf4aa5678dd3b85f2b66d48687a6f3e4995
Junior Brito <Jjunior130@gmail.com>
2015-03-05 19:11:48 -0500

    fixed duplicate keys

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject static-website "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring "1.3.2"]
