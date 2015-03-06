ddb938f6920e43ba3b7e373e9472db3570167d5d
Junior Brito <Jjunior130@gmail.com>
2015-03-05 21:38:23 -0500

    fixing the clojure icon

--- src/static_website/handler.clj
+++ src/static_website/handler.clj
@@ -15,10 +15,13 @@
                        [:h1 "Junior Brito"]
                        [:p [:em "Web Developer"]]
                        [:p "Student of programming."]
-                       [:div.icon (elem/image "Clojure_logo.gif")]
+                       [:div.icon (elem/link-to "http://clojure.org"
+                                                (elem/image "Clojure_logo.gif"))]
                        [:p "Clojure user."]
                        [:p "My first project was an Android app were
-                        you can input fat, carb, protein in either Calories or grams and the program shows you the nutritional proportion with text and a colorful pie chart."]
+                        you can input fat, carb, protein in either Calories
+                        or grams and the program shows you the nutritional
+                        proportion with text and a colorful pie chart."]
                        [:br]
                        [:br]
                        [:br]
